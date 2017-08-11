b267276c78ad82c5e713b527b9cbcb6e4bd30ec0
Michael Dinh <mkdinh94@gmail.com>
2017-08-11 14:39:45 -0400

    user signup finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   get '/signup', to: 'users#new'
 
+  post '/signup'  , to: 'users#create'
+
   get '/help', to: 'static_pages#help'
 	# route a get request for the URL /help to the help action in the staticpage controller 
 
@@ -11,4 +13,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
+  #endowed all actions for RESTful resources
 end
